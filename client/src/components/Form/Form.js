e6823bbd277729472ec6e9676a1e21441d9084e9
kais manai <kaismanai1975@gmail.com>
2022-02-25 12:47:49 +0100

    form styling

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import makeStyles from"./Styles.js"
 import FileBase from "react-file-base64"
 import { TextField,Button,Typography,Paper } from '@material-ui/core'
@@ -12,16 +12,17 @@ const Form = () => {
     const handleSubmit=()=>{
 
     }
-    const clear=()=>{}
- console.log(postData)
+    const clear=()=>{ setPostData({ creator:'', title:'', message:'', tags:'', selectedFile:''})}
+    
+    console.log(postData)
   return (
    <Paper className={classes.paper}>
       <form className={classes.form} autoComplete="off" noValidate onClick={handleSubmit}>
             <Typography variant="h6">Creating Memorie</Typography>
-            <TextField name="creator" style={{marginBottom:"10px"}} label="Creator" variant="outlined" value={postData.creator}  fullWidth honChange={e=> setPostData({...postData,creator:e.target.value})}/>
-            <TextField name="title" style={{marginBottom:"10px"}} label="title" variant="outlined" value={postData.title}  fullWidth honChange={e=> setPostData({...postData,title:e.target.value})}/>
+            <TextField name="creator" style={{marginBottom:"10px"}} label="Creator" variant="outlined" value={postData.creator}  fullWidth onChange={e=> setPostData({...postData,creator:e.target.value})}/>
+            <TextField name="title" style={{marginBottom:"10px"}} label="title" variant="outlined" value={postData.title}  fullWidth onChange={e=> setPostData({...postData,title:e.target.value})}/>
             <TextField name="message" style={{marginBottom:"10px"}} label="message" variant="outlined" value={postData.message}  fullWidth onChange={e=> setPostData({...postData,message:e.target.value})}/>
-            <TextField name="tags" style={{marginBottom:"10px"}} label="tags" variant="outlined" value={postData.tags}  fullWidth honChange={e=> setPostData({...postData,tags:e.target.value})}/>
+            <TextField name="tags" style={{marginBottom:"10px"}} label="tags" variant="outlined" value={postData.tags}  fullWidth onChange={e=> setPostData({...postData,tags:e.target.value})}/>
 
             <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={(base64)=>{setPostData({...postData,selectedFile:base64})}}/></div>
 
